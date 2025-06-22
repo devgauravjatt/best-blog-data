571817d82673d8a99c87a09224a049342cb1bd41
devgauravjatt <devgauravjatt@gmail.com>
2025-06-22 15:03:22 +0530

    feat: create BestBlogData class
    - Created a new class `BestBlogData` to encapsulate all data fetching functions.
    - This improves organization and allows for easier testing and potential future expansion.
    - Moved all existing data fetching functions into the new class.

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,28 @@ import {
 } from './lib/posts'
 import { getPostsBySearch } from './lib/search'
 
-export {
-  getAllCategories,
-  getFullPostBySlug,
-  getPosts,
-  getPostsByCategory,
-  getPostsBySearch,
+class BestBlogData {
+  constructor() {}
+
+  getAllCategories() {
+    return getAllCategories()
+  }
+
+  getFullPostBySlug(slug: string) {
+    return getFullPostBySlug(slug)
+  }
+
+  getPosts(pageIndex: number = 1) {
+    return getPosts(pageIndex)
+  }
+
+  getPostsByCategory(category: string, pageIndex: number = 1) {
+    return getPostsByCategory(category, pageIndex)
+  }
+
+  getPostsBySearch(query: string) {
+    return getPostsBySearch(query)
+  }
 }
+
+export default BestBlogData
